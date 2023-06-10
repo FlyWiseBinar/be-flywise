c1bdeaa45e5de670bc987d7f3593f2e247789299
kidman <thephantomwarrior02@gmail.com>
2023-06-10 18:28:18 +0700

    fix: deploy

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const router = require("./routes")
 
 const app = express()
 const port = process.env.PORT || 5000
+// console.log('port', process.env.PORT);
 const baseUrl = process.env.BASE_URL || `http://localhost:${port}`
 
 app.use(express.static("public"))
