ba72cb73761cbb9e70ba9118c45fd4778ced1bcc
ganden16 <alvindian85@gmail.com>
2023-06-21 18:20:07 +0700

    fix routes favorite

--- routes/danu.js
+++ routes/danu.js
@@ -4,7 +4,7 @@ const { ticketController } = require("../controller/ticket")
 
 router.get("/schedule/search", ticketController.searchScheduleMulti)
 router.get("/schedule/airport", ticketController.getAirportByName)
-router.get("/schedule/:id", ticketController.findSchedule)
 router.get("/schedule/favorite", ticketController.getScheduleFavorite)
+router.get("/schedule/:id", ticketController.findSchedule)
 
 module.exports = router
