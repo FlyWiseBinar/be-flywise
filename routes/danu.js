74ad8d674865825f510028f8b7635cc668879c56
ganden16 <alvindian85@gmail.com>
2023-06-21 16:11:46 +0700

    fix routes airport

--- routes/danu.js
+++ routes/danu.js
@@ -3,8 +3,8 @@ const router = Router()
 const { ticketController } = require("../controller/ticket")
 
 router.get("/schedule/search", ticketController.searchScheduleMulti)
-router.get("/schedule/:id", ticketController.findSchedule)
 router.get("/schedule/airport", ticketController.getAirportByName)
+router.get("/schedule/:id", ticketController.findSchedule)
 router.get("/schedule/favorite", ticketController.getScheduleFavorite)
 
 module.exports = router
